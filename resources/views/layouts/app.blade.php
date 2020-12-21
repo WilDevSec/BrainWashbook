8af3098341f624caa468f9c6936f36208741a043
wilhutchens <wilhutchens@gmail.com>
2020-12-21 00:39:43 +0000

    Add to views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <title>BrainWashBook - @yield('title')</title>
+    <title>BrainWashBook @yield('title')</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -25,17 +25,26 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    BrainWashBook - @yield('title')
+                <a class="navbar-brand active" href="{{ url('/') }}">
+                    <span class="sr-only">(current)</span>
+                    BrainWashBook
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>   
-
+                
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-
+                          <li class="nav-item">
+                            <a class="nav-link" href="/newsfeed">Latest Posts</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#">Create Post</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="/profile">Profile</a>
+                          </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -62,9 +71,10 @@
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                                     document.getElementById('logout-form').submit();">Logout
                                         {{ __('Logout') }}
                                     </a>
+                                    <a class="dropdown-item" href="#">Another action</a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
--- resources/views/pages/newsfeed.blade.php
+++ resources/views/pages/newsfeed.blade.php
@@ -8,11 +8,24 @@
     <div class="list-group list-group-flush">
         @if(count($posts)>0)
             @foreach($posts as $post)
-                <a href="#" class="list-group-item">{{$post->title}}</a>
+            <div class="col-md-6">
+                <div class="card">
+                    <a href="#" class="list-group-item">{{$post->title}}</a>
+                    <p>{{$post->body}}</p>
+                </div>
+            </div>
+                    {{-- <div class="col-md-4">
+                        <div class="card">
+                          <a href="{{url('detail/'.Str::slug($post->title).'/'.$post->id)}}"><img src="{{asset('imgs/thumb/'.$post->thumb)}}" class="card-img-top" alt="{{$post->title}}" /></a>
+                          <div class="card-body">
+                            <h5 class="card-title"><a href="{{url('detail/'.Str::slug($post->title).'/'.$post->id)}}">{{$post->title}}</a></h5>
+                          </div>
+                        </div>
+                    </div> --}}
                 
             @endforeach
         @endif
     </div>
 </div>
-
+{{$posts->links()}}
 @endsection
